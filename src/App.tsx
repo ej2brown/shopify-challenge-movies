53c64c53cb029276842af711190148af3ce517e5
Elizabeth Brown <ej2brown@uwaterloo.ca>
2020-08-26 20:53:36 -0400

    add basic input form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+
+  const [movie, setMovie] = useState('');
+  // const [error, setError] = useState("");
+
+  const handleSearchInput = (evt: any) => {
+    setMovie(evt.target.value);
+  };
+
+  const onSearch = () => {
+    console.log(movie)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      <section>
+        <form>
+          <input
+            placeholder="Enter Movie Title"
+            value={movie}
+            onChange={handleSearchInput}
+          >
+          </input>
+        </form>
+
+        <button
+          onClick={() => {
+            onSearch();
+          }}
+        >
+          Search
+        </button>
+      </section>
     </div>
   );
 }
