2b36ce77123344c95c740eca1e17f0cf37dca577
Elizabeth Brown <ej2brown@uwaterloo.ca>
2020-08-26 22:33:26 -0400

    set axios get request with omdb api key

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
 import React, { useState } from "react";
+import axios from "axios";
+
 import './App.css';
+
+const APIKEY = process.env.REACT_APP_APIKEY;
 
 function App() {
 
   const [movie, setMovie] = useState('');
-  // const [error, setError] = useState("");
+  const [results, setResults] = useState({});
 
   const handleSearchInput = (evt: any) => {
     setMovie(evt.target.value);
   };
 
   const onSearch = () => {
-    console.log(movie)
+    const queryTitle = querifyString(movie);
+    axios.get(`http://www.omdbapi.com/?apikey=${APIKEY}&t=${queryTitle}`)
+      .then((response) => {
+        console.log(response)
+        setResults(response.data)
+      })
+      .catch(error => console.log(error))
   }
+
+  const querifyString = (str: string) => {
+    return str.split(' ').reduce((result, word) => { return result + '+' + word })
+  }
+
 
   return (
     <div className="App">
       <header className="App-header">
-
       </header>
-      <section>
+      <section className="movie-input">
         <form>
           <input
             placeholder="Enter Movie Title"
@@ -36,6 +50,8 @@ function App() {
         >
           Search
         </button>
+      </section>
+      <section className="movie-results">
       </section>
     </div>
   );
