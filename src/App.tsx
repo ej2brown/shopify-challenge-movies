4307ac9222365e85a4b0dd096864022c4fbb9da9
Elizabeth Brown <ej2brown@uwaterloo.ca>
2020-08-27 17:53:17 -0400

    add function to remove nominatee

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Button from "./components/button";
 
 import './App.css';
 
@@ -21,7 +22,6 @@ function App() {
       .then((response) => {
         const result = response.data.Search;
         console.log(result);
-        // setResults(result, ...results);
         setResults(result);
       })
       .catch(error => console.log(error));
@@ -36,72 +36,75 @@ function App() {
   }
 
   const onRemoveNominate = (movie: any) => {
-    nominations.find((value: { imdbID: any; }, index: React.Key) => {
-      if (movie.imdbID === value.imdbID) {
-        console.log('Deleting nomination of:', index, nominations[index]);
-        nominations.splice(index, 1);
-        console.log(nominations)
-        setNominations(nominations)
+    const copy = [...nominations]; // make a separate copy of the array
+    const index = copy.map(function(movie) { return movie.imdbID }).indexOf(movie.imdbID);
 
-      }
-    })
-    // setNominations([nominations, ...nominations])
+    if (index !== -1) { // checks if movie has found 
+      copy.splice(index, 1);
+      setNominations([...copy]);
+    }
   }
 
   useEffect(() => {
+    console.log("useEffect change");
+    console.log('results', results);
+    console.log('nominations', nominations);
   }, [results, nominations]);
 
 
-return (
-  <div className="App">
-    {/* <header className="App-header">
+  return (
+    <div className="App">
+      {/* <header className="App-header">
       </header> */}
-    <section className="movie-input">
-      Movie Title
+      <section className="movie-input">
+        Movie Title
         <form>
-        <input
-          placeholder="Enter Movie Title"
-          value={movie}
-          onChange={handleSearchInput}
-        >
-        </input>
-      </form>
+          <input
+            placeholder="Enter Movie Title"
+            value={movie}
+            onChange={handleSearchInput}
+          >
+          </input>
+        </form>
 
-      <button
-        onClick={() => {
-          onSearch();
-        }}
-      >
-        Search
+        <button
+          onClick={() => onSearch()}
+        >
+          Search
         </button>
-    </section>
-    <section className="movie-results">
-      <h3>Results for "{movie}"</h3>
-      {results.map((movie: any, index: number) => {
-        return (
-          <ul key={`${index}`}>
-            <li>{movie.Title} ({movie.Year})</li>
-            <button onClick={() => {
-              onNominate(movie);
-            }}>Nominate</button>
-          </ul>
-        )
-      })}
-    </section>
-    <section className="movie-nominations">
-      {nominations.map((movie: any, index: number) => {
-        return (
-          <ul key={`${index}`}>
-            <li>{movie.Title} ({movie.Year})</li>
-            <button onClick={() => {
-              onRemoveNominate(movie);
-            }}>Remove</button>
-          </ul>
-        )
-      })}
-    </section>
-  </div>
-);
+      </section>
+      <section className="movie-results">
+        <h3>Results for "{movie}"</h3>
+        {results && results.map((movie: any, index: number) => {
+          return (
+            <ul key={`${index}`}>
+              <li>{movie.Title} ({movie.Year})</li>
+              <Button
+                onNominate={onNominate}
+                movie={movie}
+                onClick={() => {
+                  onNominate(movie)
+                }}
+              >Nominate</Button>
+            </ul>
+          )
+        })}
+      </section>
+      <section className="movie-nominations">
+        <h3>Nominations</h3>
+        {nominations && nominations.map((movie: any, index: number) => {
+          return (
+            <ul key={`${index}`}>
+              <li>{movie.Title} ({movie.Year})</li>
+              <button onClick={() => {
+                onRemoveNominate(movie);
+              }}>Remove</button>
+            </ul>
+          )
+        })}
+      </section>
+    </div>
+  );
 }
 
 export default App;
