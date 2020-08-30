3c083a726caa9a428f33c72614d29151fea3dece
Elizabeth Brown <ej2brown@uwaterloo.ca>
2020-08-29 20:22:25 -0400

    add toastify notification banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,77 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useEffect } from "react";
+
+import useApplicationData from "./hooks/useApplicationData";
 
 //components
 import SearchBar from "./components/searchBar";
 import ResultsList from "./components/resultsList";
 import NominationsList from "./components/nominationsList";
+import { ToastContainer, toast } from 'react-toastify';
 
-import './App.css';
-
-const APIKEY = process.env.REACT_APP_APIKEY;
+import 'react-toastify/dist/ReactToastify.css';
+import './App.scss';
 
 export default function App() {
 
-  const [movie, setMovie] = useState('Guardians of the Galaxy Vol. 2'); // for testing 
-  const [results, setResults] = useState([] as any);
-  const [nominations, setNominations] = useState([] as any);
+  const {
+    movie,
+    results,
+    nominations,
+    onSearch,
+    handleSearchInput,
+    onNominate,
+    onRemoveNominate
+  } = useApplicationData();
 
-  const handleSearchInput = (evt: any) => {
-    setMovie(evt.target.value);
+
+  const notify = () => {
+    toast.success(`You have nominated 5 movies!`, {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
   };
-
-  const onSearch = () => {
-    const queryTitle = querifyString(movie);
-    axios.get(`http://www.omdbapi.com/?apikey=${APIKEY}&s=${queryTitle}`)
-      .then((response) => {
-        const result = response.data.Search;
-        console.log(result);
-        setResults(result);
-      })
-      .catch(error => console.log(error));
-  }
-
-  const querifyString = (str: string) => {
-    return str.split(' ').reduce((result, word) => { return result + '+' + word });
-  }
-
-  const onNominate = (movie: any) => {
-    setNominations([movie, ...nominations])
-  }
-
-  const onRemoveNominate = (movie: any) => {
-    const copy = [...nominations]; // make a separate copy of the array
-    const index = copy.map(function(movie) { return movie.imdbID }).indexOf(movie.imdbID);
-
-    if (index !== -1) { // checks if movie has found 
-      copy.splice(index, 1);
-      setNominations([...copy]);
-    }
-  }
-
+  
   useEffect(() => {
-    console.log("useEffect change");
-    console.log('results', results);
-    console.log('nominations', nominations);
+    if (nominations.length === 5) {
+      notify()
+    };
   }, [results, nominations]);
-
 
   return (
     <div className="App">
-      {/* <header className="App-header">
-      </header> */}
+      <header className="App-header">
+        <h1>The Shoppies</h1>
+      </header>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <SearchBar
         movie={movie}
         handleSearchInput={handleSearchInput}
         onSearch={onSearch} />
-      <ResultsList 
-      movie={movie} 
-      results={results} 
-      nominations={nominations}
-      onNominate={onNominate} 
+      <ResultsList
+        movie={movie}
+        results={results}
+        nominations={nominations}
+        onNominate={onNominate}
       />
-      <NominationsList 
-      nominations={nominations}
-      onRemoveNominate={onRemoveNominate}
-            />
+      <NominationsList
+        nominations={nominations}
+        onRemoveNominate={onRemoveNominate}
+      />
     </div>
   );
 };
