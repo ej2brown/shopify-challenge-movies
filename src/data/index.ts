7bf1a167097369fb79214abf5c0a1d513330f381
Elizabeth Brown <ej2brown@uwaterloo.ca>
2020-08-29 20:17:28 -0400

    add hardcoded data

---
+++ src/data/index.ts
@@ -0,0 +1,25 @@
+const hardCodedData = [
+  {
+    Poster: "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg",
+    Title: "Guardians of the Galaxy Vol. 1",
+    Type: "movie",
+    Year: "2017",
+    imdbID: "1",
+  },
+  {
+    Poster: "N/A",
+    Title: "Bonus Round: The Making of 'Guardians of the Galaxy Vol. 2'",
+    Type: "movie",
+    Year: "2017",
+    imdbID: "2",
+
+  },
+  {
+    Poster: "N/A",
+    Title: "Chris Pratt Shows You Around the Set of Guardians of the Galaxy Vol. 3",
+    Type: "movie",
+    Year: "2016",
+    imdbID: "3"
+  }
+]
+export default hardCodedData
