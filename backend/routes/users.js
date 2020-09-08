3ad2700f5cde69758b075e61b7b1964cab25a8d2
Elizabeth Brown <ej2brown@uwaterloo.ca>
2020-09-07 22:43:48 -0400

    Add routes for nominations

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -22,7 +22,6 @@ module.exports = (db) => {
       )
       .then(data => {
         const users = data.rows[0];
-        console.log(users);
         res.json(users);
       })
       .catch(err => {
@@ -53,6 +52,77 @@ module.exports = (db) => {
       })
       .catch(err => {
         res.status(500).json({ isValid: false, error: err.message });
+      });
+  });
+
+
+  router.get("/nominations/:emailId", (req, res) => {
+    const emailId = req.params.emailId;
+    console.log(emailId);
+    db
+      .query(
+        `SELECT * FROM nominations
+          WHERE email_id=$1;
+        `,
+        [emailId]
+      )
+      .then(data => {
+        const nominations = data.rows;
+        res.json(nominations);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.post("/nominate", (req, res) => {
+    const { emailId, imdbID, Title, Year } = req.body;
+    if (!emailId) {
+      return res.status(400).send({ isValid: false, error: "User not found." });
+    }
+    db
+      .query(
+        `
+          INSERT INTO nominations
+          (email_id, imdbID, title, year)
+          VALUES
+          ($1, $2, $3, $4)
+          RETURNING *;
+        `,
+        [emailId, imdbID, Title, Year]
+      )
+      .then(() => {
+        res.status(200).json({ isValid: true });
+      })
+      .catch(err => {
+        res.status(500).json({ isValid: false, error: err.message });
+      });
+  });
+
+  router.get("/nominations/:emailId/:imdbId", (req, res) => {
+    const emailId = req.params.emailId;
+    const imdbId = req.params.imdbId;
+    db
+      .query(
+        `SELECT * FROM nominations
+         WHERE email_id=$1 AND imdbid=$2;
+        `,
+        [emailId, imdbId]
+      )
+      .then(data => {
+        console.log(data);
+        if (data.rows) {
+          return res.json(data.rows);
+        } else {
+          return null;
+        }
+      })
+      .catch(err => {
+        res
+          .status(200)
+          .json({ error: err.message });
       });
   });
 
