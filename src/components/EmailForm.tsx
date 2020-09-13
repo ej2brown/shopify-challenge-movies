a653a55d92aea6feea521e9a13523604207025d7
Elizabeth Brown <ej2brown@uwaterloo.ca>
2020-09-13 12:52:30 -0400

    wrap in a section tag for css

--- src/components/EmailForm.tsx
+++ src/components/EmailForm.tsx
@@ -35,34 +35,36 @@ export default function LoginForm(props: any) {
   };
 
   return (
-    <Container className="email-form">
-      <Typography component="h4" variant="h6">
-        Get/Save Your Nominations!
+    <section className="email-form">
+      <Container >
+        <Typography component="h4" variant="h6">
+          Get/Save Your Nominations!
         </Typography>
-      <form onSubmit={handleSubmit}>
-        <Grid container spacing={1}>
-          <Grid item xs={12}>
-            <TextField
-              required
-              fullWidth
-              variant="outlined"
-              id="email"
-              type="email"
-              label="Email Address"
-              value={email}
-              onChange={event => setEmail(event.target.value)}
-            />
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                variant="outlined"
+                id="email"
+                type="email"
+                label="Email Address"
+                value={email}
+                onChange={event => setEmail(event.target.value)}
+              />
+            </Grid>
           </Grid>
-        </Grid>
-        <Button
-          fullWidth
-          type="submit"
-          variant="contained"
-          color="primary">Submit</Button>
-      </form>
-      {error && <div>The email provided was incorrect!</div>}
-      {user && <Button variant="outlined" fullWidth color="primary" onClick={() => fetchNominations(user.id)}>fetch your nominations</Button>}
-      {user && <Button variant="outlined" fullWidth color="primary" onClick={() => postNominations(user.id, nominations)}>set your nominations</Button>}
-    </Container >
+          <Button
+            fullWidth
+            type="submit"
+            variant="contained"
+            color="primary">Submit</Button>
+        </form>
+        {error && <div>The email provided was incorrect!</div>}
+        {user && <Button variant="outlined" fullWidth color="primary" onClick={() => fetchNominations(user.id)}>fetch your nominations</Button>}
+        {user && <Button variant="outlined" fullWidth color="primary" onClick={() => postNominations(user.id, nominations)}>set your nominations</Button>}
+      </Container >
+    </section>
   )
 }
