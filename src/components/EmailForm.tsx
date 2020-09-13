69d5563e198460bd3a662745e566a3d0567d6bcf
Elizabeth Brown <ej2brown@uwaterloo.ca>
2020-09-13 02:58:38 -0400

    Add fullwidth

--- src/components/EmailForm.tsx
+++ src/components/EmailForm.tsx
@@ -61,8 +61,8 @@ export default function LoginForm(props: any) {
           color="primary">Submit</Button>
       </form>
       {error && <div>The email provided was incorrect!</div>}
-      {user && <Button variant="outlined" color="primary" onClick={() => fetchNominations(user.id)}>fetch your nominations</Button>}
-      {user && <Button variant="outlined" color="primary" onClick={() => postNominations(user.id, nominations)}>set your nominations</Button>}
+      {user && <Button variant="outlined" fullWidth color="primary" onClick={() => fetchNominations(user.id)}>fetch your nominations</Button>}
+      {user && <Button variant="outlined" fullWidth color="primary" onClick={() => postNominations(user.id, nominations)}>set your nominations</Button>}
     </Container >
   )
 }
